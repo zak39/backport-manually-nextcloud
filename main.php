1eeed4c6fb578e724dacd405283ade4f751c80d8
Baptiste Fotia <fotia.baptiste@hotmail.com>
2023-01-23 16:14:34 +0100

    fix(PHP): Define the old
    Signed-off-by: Baptiste Fotia <fotia.baptiste@hotmail.com>

--- main.php
+++ main.php
@@ -4,14 +4,28 @@ class JonSnow {
     
     private string $firstname;
     private string $lastname;
+    private int $old;
 
     public function __construct(
         $firstname,
-        $lastname
+        $lastname,
+        $old
     )
     {
         $this->firstname = $firstname;
         $this->lastname = $lastname;
+        $this->old = $old;
+    }
+
+
+    public function getOld(): int
+    {
+        return $this->old;
+    }
+
+    public function setOld(int $old): void
+    {
+        $this->old = $old;
     }
 
     public function saySpeech()
@@ -26,7 +40,10 @@ class JonSnow {
 
 }
 
-$jon = new JonSnow('Jon', 'Snow');
+$jon = new JonSnow('Jon', 'Snow', 25);
 
 print($jon->getFullName() . "\n");
 print($jon->saySpeech() . "\n");
+print(strval($jon->getOld()) . "\n");
+$jon->setOld(26);
+print(strval($jon->getOld()) . "\n");
