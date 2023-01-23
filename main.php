4280118e7cff6701f5b844346d40dd0f129b49c0
Baptiste Fotia <fotia.baptiste@hotmail.com>
2023-01-23 15:54:15 +0100

    first commit
    Signed-off-by: Baptiste Fotia <fotia.baptiste@hotmail.com>

---
+++ main.php
@@ -0,0 +1,32 @@
+<?php
+
+class JonSnow {
+    
+    private string $firstname;
+    private string $lastname;
+
+    public function __construct(
+        $firstname,
+        $lastname
+    )
+    {
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+    }
+
+    public function saySpeech()
+    {
+        return "Winter is comming... The death come";
+    }
+
+    public function getFullName()
+    {
+        return $this->firstname . " " . $this->lastname;
+    }
+
+}
+
+$jon = new JonSnow('Jon', 'Snow');
+
+print($jon->getFullName() . "\n");
+print($jon->saySpeech() . "\n");
